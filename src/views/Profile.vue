27134df7e1af77d3514afafc3ea79602e460a86a
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-04-27 07:58:02 -0300

    fix: ajustes

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -126,6 +126,293 @@ Estudante de Nutrição - Unicamp Limeira              </div>
           </div>
         </div>
       </section>
+      <section class="relative py-16 bg-blueGray-200">
+        <div class="container mx-auto px-4">
+          <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg"
+          >
+            <div class="px-6">
+              <div class="flex flex-wrap justify-center">
+                <div
+                  class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
+                >
+                  <div class="relative">
+                    <img
+                      alt="..."
+                      src="img-nutrition/juliagomes.png"
+                      class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                    />
+                  </div>
+                </div>
+                <div
+                  class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"
+                >
+                  
+                </div>
+                <div class="w-full lg:w-4/12 px-4 lg:order-1">
+                  <div class="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="lg:mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="text-center mt-24">
+                <h3
+                  class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+                >
+                  Júlia Gomes Brandão Clini
+
+
+                </h3>
+                <div
+                  class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
+                >
+                  <i
+                    class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"
+                  ></i>
+                 Jundiaí - SP
+                </div>
+                <div class="mb-2 text-blueGray-600 mt-10">
+                  <i
+                    class="fas fa-briefcase mr-2 text-lg text-blueGray-400"
+                  ></i>
+Estudante de Nutrição - Unicamp Limeira              </div>
+               
+              </div>
+              <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+                <div class="flex flex-wrap justify-center">
+                  <div class="w-full lg:w-9/12 px-4">
+                    <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
+ Tenho 19 anos e sou de Jundiaí - SP. Estou me encantando cada dia mais pelo hábito de cozinhar e provar coisas novas, principalmente com as aulas práticas do curso de Nutrição. Gosto também de aproveitar a companhia de quem amo e assistir séries.
+                   </p>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-normal text-emerald-500"
+                    >
+                      Show more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="relative py-16 bg-blueGray-200">
+        <div class="container mx-auto px-4">
+          <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg"
+          >
+            <div class="px-6">
+              <div class="flex flex-wrap justify-center">
+                <div
+                  class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
+                >
+                  <div class="relative">
+                    <img
+                      alt="..."
+                      src="img-nutrition/lais.png"
+                      class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                    />
+                  </div>
+                </div>
+                <div
+                  class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"
+                >
+                  
+                </div>
+                <div class="w-full lg:w-4/12 px-4 lg:order-1">
+                  <div class="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="lg:mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="text-center mt-24">
+                <h3
+                  class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+                >
+                  Laís Bordini Mestriner
+
+
+                </h3>
+                <div
+                  class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
+                >
+                  <i
+                    class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"
+                  ></i>
+                 Jundiaí - SP
+                </div>
+                <div class="mb-2 text-blueGray-600 mt-10">
+                  <i
+                    class="fas fa-briefcase mr-2 text-lg text-blueGray-400"
+                  ></i>
+Estudante de Nutrição - Unicamp Limeira              </div>
+               
+              </div>
+              <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+                <div class="flex flex-wrap justify-center">
+                  <div class="w-full lg:w-9/12 px-4">
+                    <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
+Tenho 18 anos, sou de Jundiaí - SP. Gosto de usar meu tempo livre para ler e assistir séries. Adoro cozinhar, principalmente receitas diferentes no final de semana!
+
+                   </p>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-normal text-emerald-500"
+                    >
+                      Show more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="relative py-16 bg-blueGray-200">
+        <div class="container mx-auto px-4">
+          <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg"
+          >
+            <div class="px-6">
+              <div class="flex flex-wrap justify-center">
+                <div
+                  class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center"
+                >
+                  <div class="relative">
+                    <img
+                      alt="..."
+                      src="img-nutrition/luana.jpg"
+                      class="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16 max-w-150-px"
+                    />
+                  </div>
+                </div>
+                <div
+                  class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center"
+                >
+                  
+                </div>
+                <div class="w-full lg:w-4/12 px-4 lg:order-1">
+                  <div class="flex justify-center py-4 lg:pt-4 pt-8">
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                    <div class="lg:mr-4 p-3 text-center">
+                      <span
+                        class="text-xl font-bold block uppercase tracking-wide text-blueGray-600"
+                      >
+                        
+                      </span>
+                      <span class="text-sm text-blueGray-400"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="text-center mt-24">
+                <h3
+                  class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2"
+                >
+                  Luana Domingos Casagrande
+
+
+
+                </h3>
+                <div
+                  class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase"
+                >
+                  <i
+                    class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"
+                  ></i>
+                 Rio Claro - SP
+                </div>
+                <div class="mb-2 text-blueGray-600 mt-10">
+                  <i
+                    class="fas fa-briefcase mr-2 text-lg text-blueGray-400"
+                  ></i>
+Estudante de Nutrição - Unicamp Limeira              </div>
+               
+              </div>
+              <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+                <div class="flex flex-wrap justify-center">
+                  <div class="w-full lg:w-9/12 px-4">
+                    <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
+Tenho 21 anos e sou de Rio Claro - SP. Gosto de cozinhar, principalmente receitas doces, e amo muito ler livros.
+
+
+
+                   </p>
+                    <a
+                      href="javascript:void(0)"
+                      class="font-normal text-emerald-500"
+                    >
+                      Show more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
     <footer-component />
   </div>
